0ea7e7d7d9dd904a21370d34ba723b51cb985a4d
Arash Dehghanian <asc_arash@yahoo.com>
2022-09-16 17:36:12 +0430

    Interaction UI Feedback
    - Added UI feedback logic to SInteractionComponent so that when player approaches any interactable objects, a widget pops up

--- Source/ActionRoguelike/Private/SInteractionComponent.cpp
+++ Source/ActionRoguelike/Private/SInteractionComponent.cpp
@@ -4,15 +4,24 @@
 #include "DrawDebugHelpers.h"
 #include "SCharacter.h"
 #include "SGameplayInterface.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget/SWorldUserWidget.h"
 
 static TAutoConsoleVariable<bool> CVarDrawDebugInteractions(TEXT("ara.DrawDebugInteractions"), false, TEXT("Enable debug lines for Interaction component"), ECVF_Cheat);
 
 USInteractionComponent::USInteractionComponent()
 {
-	
+	PrimaryComponentTick.bCanEverTick = true;
 }
 
-void USInteractionComponent::PrimaryInteract()
+void USInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	FindBestInteractable();
+}
+
+void USInteractionComponent::FindBestInteractable()
 {
 	ASCharacter* MyOwner = Cast<ASCharacter>(GetOwner());
 
@@ -24,36 +33,75 @@ void USInteractionComponent::PrimaryInteract()
 	FHitResult HitResult{};
 
 	FVector TraceStart{MyOwner->GetPawnViewLocation() + (MyOwner->GetControlRotation().Vector() * 30.0f)};
-	FVector TraceEnd{TraceStart + (MyOwner->GetControlRotation().Vector() * 1000.0f)};
+	FVector TraceEnd{TraceStart + (MyOwner->GetControlRotation().Vector() * TraceDistance)};
 
 	FCollisionObjectQueryParams ObjectQueryParams{};
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(TraceCollisionChannel);
 
 
 	FCollisionShape CollisionShape;
-	float ShapeRadius{30.0f};
-	CollisionShape.SetSphere(ShapeRadius);
+	CollisionShape.SetSphere(TraceRadius);
 
-	
+
 	bool DidItHit = GetWorld()->SweepSingleByObjectType(HitResult, TraceStart, TraceEnd, FQuat::Identity, ObjectQueryParams, CollisionShape);
-	
+
 	FColor DebugShapeColor = DidItHit ? FColor::Green : FColor::Red;
-	
+
 	bool bDrawDebug = CVarDrawDebugInteractions.GetValueOnGameThread();
+
+	FocusedActor = nullptr;
 
 	if (AActor* HitActor = HitResult.GetActor())
 	{
-		if (HitActor->Implements<USGameplayInterface>()) //whenever we want to check or cast, we use U
+		if (HitActor->Implements<USGameplayInterface>()) //whenever we want to check or cast to an Interface, we use U
 		{
-			ISGameplayInterface::Execute_Interact(HitActor, MyOwner); //whenever we want to call it, we use I
-			if(bDrawDebug)
+			FocusedActor = HitActor;
+			if (bDrawDebug)
 			{
-				DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, ShapeRadius, 16, DebugShapeColor, false, 2.0f);
+				DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, TraceRadius, 16, DebugShapeColor, false, 2.0f);
 			}
 		}
 	}
-	if(bDrawDebug)
+	
+	if (bDrawDebug)
 	{
 		DrawDebugLine(GetWorld(), TraceStart, TraceEnd, DebugShapeColor, false, 2.0f, 0, 2);
 	}
+
+	if(FocusedActor)
+	{
+		if(!PopupWidget && ensure(PopupWidgetClass))
+		{
+			PopupWidget = CreateWidget<USWorldUserWidget>(GetWorld(), PopupWidgetClass);
+		}
+		
+		if(PopupWidget)
+		{
+			PopupWidget->AttachedActor = FocusedActor;
+			
+			if(!PopupWidget->IsInViewport())
+			{
+				PopupWidget->AddToViewport();
+			}
+		}
+	}
+	else
+	{
+		if(PopupWidget)
+		{
+			PopupWidget->RemoveFromParent();
+		}
+	}
+}
+
+void USInteractionComponent::PrimaryInteract()
+{
+	if(!FocusedActor)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("No FocusedActor to interact."));
+		return;
+	}
+	
+	APawn* MyPawn = Cast<APawn>(GetOwner());
+	ISGameplayInterface::Execute_Interact(FocusedActor, MyPawn); //whenever we want to call an Interface, we use I
 }
--- Source/ActionRoguelike/Public/SInteractionComponent.h
+++ Source/ActionRoguelike/Public/SInteractionComponent.h
@@ -6,13 +6,41 @@
 #include "Components/ActorComponent.h"
 #include "SInteractionComponent.generated.h"
 
+class USWorldUserWidget;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API USInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
+	
 public:
 	USInteractionComponent();
+
+protected:
+	UPROPERTY()
+	AActor* FocusedActor{};
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceDistance{500.0f};
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceRadius{30.0f};
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	TEnumAsByte<ECollisionChannel> TraceCollisionChannel{ECC_WorldDynamic};
+
 	
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<USWorldUserWidget> PopupWidgetClass{};
+
+	UPROPERTY()
+	USWorldUserWidget* PopupWidget{};
+
+protected:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void FindBestInteractable();
+	
+public:
 	void PrimaryInteract();
+	
 };
